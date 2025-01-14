fa46a892067c4e8f160eed2a6c1b52f071cc857b
Ammar Arif <evergreenkary@gmail.com>
2025-01-14 16:21:42 -0500

    feat(blockifier): hardcode default fee token on `katana` rollup

--- crates/rpc-replay/src/block_context.rs
+++ crates/rpc-replay/src/block_context.rs
@@ -51,15 +51,22 @@ pub fn build_block_context(
 
     let chain_info = ChainInfo {
         chain_id,
-        // cf. https://docs.starknet.io/tools/important-addresses/
         fee_token_addresses: FeeTokenAddresses {
             strk_fee_token_address: contract_address!(
-                "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d"
+                "0x2e7442625bab778683501c0eadbc1ea17b3535da040a12ac7d281066e915eea"
             ),
             eth_fee_token_address: contract_address!(
-                "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7"
+                "0x2e7442625bab778683501c0eadbc1ea17b3535da040a12ac7d281066e915eea"
             ),
-        },
+        }, /* cf. https://docs.starknet.io/tools/important-addresses/
+            * fee_token_addresses: FeeTokenAddresses {
+            *     strk_fee_token_address: contract_address!(
+            *         "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d"
+            *     ),
+            *     eth_fee_token_address: contract_address!(
+            *         "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7"
+            *     ),
+            * }, */
     };
 
     // @kariy: We use the latest versioned constants to match what Katana is using.
